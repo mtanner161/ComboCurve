c777cbb94a220f12cdf2ad1cb5e5baeea1bcaffc
Michael Tanner <m.tanner161@gmail.com>
2021-12-20 12:54:17 -0700

    Working code - returns dataObj from Json.  Next step is to parse json or wait for dev team for GET res cat monthly pull

--- ccTestEcon.py
+++ ccTestEcon.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from requests.models import Response
 
+
 # connect to service account
 service_account = ServiceAccount.from_file(
     ".\king\combocurve\ComboCurve\ext-api-kingoperating2dev-account-key.json\ext-api-kingoperating2dev-account-key.json"
@@ -32,12 +33,35 @@ econId = row["id"]
 
 print(econId)
 
+auth_headers = combocurve_auth.get_auth_headers()
+urlone = (
+    "https://api.combocurve.com/v1/projects/61a92c8f34254c0013cacf3e/scenarios/61a93338b763c20015f3f68f/econ-runs/"
+    + econId
+    + "/monthly-exports"
+)
 
-# dataObj = json.load(jsonStr)
-# numWells = len(dataObj)
-# for x in range(0, numWells):
-#     row = dataObj[x]
-#     print(row["well"], " ", row["date"], " ", row["oil"], " ", row["gas"])
+response = requests.request("POST", urlone, headers=auth_headers)
 
-# df = pd.json_normalize(response.json())
-# df.to_csv("datafile.csv", encoding="utf-8", index=False)
+jsonStr = response.text
+dataObjEconRunId = json.loads(jsonStr)
+row = dataObjEconRunId
+econRunId = row["id"]
+
+auth_headers = combocurve_auth.get_auth_headers()
+urltwo = (
+    "https://api.combocurve.com/v1/projects/61a92c8f34254c0013cacf3e/scenarios/61a93338b763c20015f3f68f/econ-runs/"
+    + econId
+    + "/monthly-exports/"
+    + econRunId
+    + "?skip=0"
+)
+
+response = requests.request("GET", urltwo, headers=auth_headers)
+jsonStr = response.text
+print(len(jsonStr))
+print(jsonStr)
+
+print(econRunId)
+
+
+dataObj = json.loads(jsonStr)
