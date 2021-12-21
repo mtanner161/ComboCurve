bfd9ad038405876c756831f9d58c9c5ef38d138f
Michael Tanner <m.tanner161@gmail.com>
2021-12-21 11:58:15 -0700

    Working and outputing correct format for PowerBi

--- econOneLinerCC.py
+++ econOneLinerCC.py
@@ -16,14 +16,23 @@ combocurve_auth = ComboCurveAuth(service_account, api_key)
 
 # Call stack to get econ oneliner
 
+# set project and scenerioId - GET FROM ComboCurve
+projectId = "61a92c8f34254c0013cacf3e"
+scenarioId = "61a93338b763c20015f3f68f"
+
 # Auethenticate the client
 auth_headers = combocurve_auth.get_auth_headers()
 # URl econid
-url = "https://api.combocurve.com/v1/projects/61a92c8f34254c0013cacf3e/scenarios/61a93338b763c20015f3f68f/econ-runs"
+url = (
+    "https://api.combocurve.com/v1/projects/"
+    + projectId
+    + "/scenarios/"
+    + scenarioId
+    + "/econ-runs"
+)
 
-response = requests.request(
-    "GET", url, headers=auth_headers
-)  # GET request to pull economic ID for next query
+# GET request to pull economic ID for next query
+response = requests.request("GET", url, headers=auth_headers)
 
 jsonStr = response.text  # convert to JSON string
 dataObjBetter = json.loads(jsonStr)  # pass to data object - allows for parsing
@@ -31,7 +40,6 @@ row = dataObjBetter[0]  # sets row equal to first string set (aka ID)
 econId = row["id"]  # set ID equal to variable
 
 print(econId)  # check that varaible is passed correctly
-
 
 # Reautenticated client
 auth_headers = combocurve_auth.get_auth_headers()
@@ -54,10 +62,9 @@ print(dataObj)
 temp = dataObj[0]
 temp2 = temp["output"]  # extract output
 
-df = pd.json_normalize(response.json())
-df = df.transpose()
-dfNP = df.to_numpy()
-
+# print out type of each to help with parsing to CSV
+for key, value in temp2.items():
+    print(key, type(value))
 
 # create file pointer and set to write mode
 fp = open(
@@ -65,24 +72,27 @@ fp = open(
     "w",
 )
 
-# header
+# write header
 fp.write("Output, Value\n")
 
-# loops through each item in
+# loop through each variable and write - mulitplying floats by 1000 to correct units
 for key, value in temp2.items():
-    if type(value) != str and type(value) != NoneType:
-        if key == "firstDiscountRoi" or key == "irr":
-            fred = key + "," + str(float(value)) + "\n"
-            fp.write(fred)
-        else:
-            fred = key + "," + str(float(value) * 1000) + "\n"
-            fp.write(fred)
-    elif type(value) == NoneType:
-        continue
+    # if value is a float - rescale to $ and write to new line
+    if type(value) == float:
+        fred = key + "," + str(float(value) * 1000) + "\n"
+        fp.write(fred)
+    # just prints the other values to
     else:
-        fred = key + "," + value + "\n"
+        fred = key + "," + str(value) + "\n"
+        fp.write(fred)
 
-fp.close()
+fp.close()  # close the file pointer
 
+
+## working on doing some analytics, will need an array for that
 df = pd.json_normalize(response.json())
 df = df.transpose()
+dfNP = df.to_numpy()
+
+print("Yes")
+print("Yay")
