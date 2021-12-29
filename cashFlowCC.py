7f8cf48fcc11cb71e32e75dbb24c9c8cdb9f4ac6
Michael Tanner <m.tanner161@gmail.com>
2021-12-29 15:26:11 -0700

    Working on rolling up cash flow.  Sucessful, missing pagination in order to take more than 200 entires

--- cashFlowCC.py
+++ cashFlowCC.py
@@ -6,18 +6,20 @@
 # testgit adding anthor line
 # import packages
 from combocurve_api_v1 import ServiceAccount, ComboCurveAuth
+from combocurve_api_v1.pagination import get_next_page_url
 import requests
 import numpy as np
 import json
 import pandas as pd
 from requests.models import Response
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 # connect to service account
-service_account = ServiceAccount.from_file(
-    ".\king\combocurve\ComboCurve\ext-api-kingoperating2dev-account-key.json\ext-api-kingoperating2dev-account-key.json"
-)
-api_key = "AIzaSyB8nd5sPJJNGwC94XufF4iliqNyDZHGjw8"  # set API Key
+service_account = ServiceAccount.from_file(os.getenv("API_SEC_CODE"))
+api_key = os.getenv("API_KEY_PASS")  # set API Key from enviroment variable
 # specific Python ComboCurve authentication
 combocurve_auth = ComboCurveAuth(service_account, api_key)
 
@@ -66,6 +68,7 @@ urltwo = (
     + "/monthly-exports/"
     + econRunId
     + "?skip=0"
+    + "&take=200"
 )
 
 # same as above, parsing as JSON string
@@ -74,3 +77,126 @@ jsonStr = response.text
 print(len(jsonStr))
 # print(jsonStr)
 dataObj = json.loads(jsonStr)
+
+dataResults = dataObj["results"]
+numEntries = len(dataResults)
+
+### Working on rolling up monthly cash flow in a Res Cat Monthly Output
+
+# lists for each of the columns I need rolled up
+grossOilWellHeadVolumeTable = []
+grossGasWellHeadVolumeTable = []
+oilRevenueTable = []
+gasRevenueTable = []
+totalNetRevenueTable = []
+totalExpenseTable = []
+netIncomeTable = []
+totalCapexTable = []
+beforeIncomeTaxCashFlowTable = []
+dateTable = []
+
+# Setting row, wellId and date to correct values
+for i in range(0, numEntries):
+    row = dataResults[i]
+    wellId = row["well"]
+    output = row["output"]
+    date = row["date"]
+    # getting the total variables casted as floats for addition
+    totalGrossOilVolume = float(output["grossOilWellHeadVolume"])
+    totalNetRevenue = float(output["totalRevenue"])
+    totalGrossGasVolume = float(output["grossGasWellHeadVolume"])
+    oilRev = float(output["oilRevenue"])
+    gasRev = float(output["gasRevenue"])
+    totalExpense = float(output["totalExpense"])
+    netIncome = float(output["netIncome"])
+    totalCapex = float(output["totalCapex"])
+    biCashFlow = float(output["beforeIncomeTaxCashFlow"])
+
+    # loop to confirm new well and same date
+    for j in range(i + 1, numEntries):
+        row2 = dataResults[j]
+        wellId2 = row2["well"]
+        date2 = row2["date"]
+        # check to make sure wellID ISNT the same and the date is, then calculate all date
+        if (wellId2 != wellId) and (date2 == date):
+            output = row2["output"]
+            totalGrossOilVolume = totalGrossOilVolume + float(
+                output["grossOilWellHeadVolume"]
+            )
+            totalNetRevenue = totalNetRevenue + float(output["totalRevenue"])
+            totalGrossGasVolume = totalGrossGasVolume + float(
+                output["grossGasWellHeadVolume"]
+            )
+            oilRev = oilRev + float(output["oilRevenue"])
+            gasRev = gasRev + float(output["gasRevenue"])
+            totalExpense = totalExpense + float(output["totalExpense"])
+            netIncome = netIncome + float(output["netIncome"])
+            totalCapex = totalCapex + float(output["totalCapex"])
+            biCashFlow = biCashFlow + float(output["beforeIncomeTaxCashFlow"])
+    # counter to confirm same date
+    dateCount = dateTable.count(date)
+    # if dateCount is 0, then add each new summed variable to new list
+    if dateCount == 0:
+        dateTable.append(date)
+        grossOilWellHeadVolumeTable.append(totalGrossOilVolume)
+        totalNetRevenueTable.append(totalNetRevenue)
+        grossGasWellHeadVolumeTable.append(totalGrossGasVolume)
+        oilRevenueTable.append(oilRev)
+        gasRevenueTable.append(gasRev)
+        totalExpenseTable.append(totalExpense)
+        netIncomeTable.append(netIncome)
+        totalCapexTable.append(totalCapex)
+        beforeIncomeTaxCashFlowTable.append(biCashFlow)
+
+
+# Begins printing the clean CSV
+fp = open(
+    r"C:\Users\MichaelTanner\Documents\code_doc\king\combocurve\ComboCurve\cashFlowRollUpTest.csv",
+    "w",
+)
+
+# writes the header
+headerString = (
+    "Date,"
+    + "Total Gross Oil Volume (bbl),"
+    + "Total Net Reveune ($),"
+    + "Total Gross Gas Volume (MCF),"
+    + "Oil Revenue ($),"
+    + "Gas Revenue ($),"
+    + "Total Expenses ($),"
+    + "Net Income ($),"
+    + "Total CAPEX ($),"
+    + "Before Income Tax Cash Flow ($)"
+    + "\n"
+)
+fp.write(headerString)
+
+# writes specific variables to correct CSV cell
+for i in range(0, len(dateTable)):
+    var = (
+        str(dateTable[i])
+        + ","
+        + str(grossOilWellHeadVolumeTable[i])
+        + ","
+        + str(totalNetRevenueTable[i])
+        + ","
+        + str(grossGasWellHeadVolumeTable[i])
+        + ","
+        + str(oilRevenueTable[i])
+        + ","
+        + str(gasRevenueTable[i])
+        + ","
+        + str(totalExpenseTable[i])
+        + ","
+        + str(netIncomeTable[i])
+        + ","
+        + str(totalCapexTable[i])
+        + ","
+        + str(beforeIncomeTaxCashFlowTable[i])
+        + "\n"
+    )
+    fp.write(var)
+
+fp.close()
+
+print("yay")
