bd03c6dbe538d7be312ecd70aff4a9cd0e0a4a39
Michael Tanner <m.tanner161@gmail.com>
2022-01-12 08:06:27 -0700

    adding the email functionalitly

--- greasebookClean.py
+++ greasebookClean.py
@@ -7,10 +7,11 @@ import json
 import pandas as pd
 import numpy as np
 import re
+import smtplib
 
 # get raw CSV download from working direcotry
 rawProduction = pd.read_excel(
-    r"C:\Users\MichaelTanner\OneDrive - Sandstone Group\Clients\King Operating\Finance - Reservoir\Daily Production Data\1.10.2022greasebook.xlsx"
+    r"C:\Users\MichaelTanner\OneDrive - Sandstone Group\Clients\King Operating\Finance - Reservoir\Daily Production Data\1.11.2022greasebook.xlsx"
 )
 
 # setting the headers
@@ -79,6 +80,44 @@ for i in range(0, len(rawProduction)):
     fp.write(outputString)
 
 fp.close()
+
+cleanKellyAsset = pd.read_csv(
+    r"C:\Users\MichaelTanner\Documents\code_doc\king\data\kellyAssets.csv"
+)
+
+todayDate = "1/11/2022"
+oilSum = 0
+gasSum = 0
+
+for i in range(0, len(cleanKellyAsset)):
+    if cleanKellyAsset.iloc[i, 0] == todayDate:
+        gasSum = gasSum + float(cleanKellyAsset.iloc[i, 5])
+        oilSum = oilSum + float(cleanKellyAsset.iloc[i, 4])
+    else:
+        continue
+
+
+print(oilSum)
+print(gasSum)
+
+dashboardLink = "https://app.powerbi.com/view?r=eyJrIjoiM2U5OTYxOWYtOTEyMS00M2YxLWE0NTktMDFjZjcwNzlmMjg3IiwidCI6IjA1MTM5NTUzLWVlOTAtNDdhZi1iNmY3LTU0ZDk2OTc4ZTQ5ZSJ9&pageName=ReportSectionb8f3ed9f3c4313759775"
+
+
+from mailer import Mailer
+
+mail = Mailer(email="operations.king@gmail.com", password="Bigshow1637%")
+
+mail.send(
+    receiver="mtanner@kingoperating.com, kduncan@kingoperating.com, pgerome@kingoperating.com",
+    subject="Daily Production Report: 1/11/2021 - ETX, STX and Gulf Coast",
+    message="Today's oil production: "
+    + str(oilSum)
+    + " bbl. \n\n"
+    + "Daily gas production: "
+    + str(gasSum)
+    + " mcf. \n\nView the Dashboard here: "
+    + dashboardLink,
+)
 
 
 print("done")
